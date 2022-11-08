87c60c228436de764759c3b9a3fab833b0f52f0c
Sabrina Liberas <sabrina.liberas@hotmail.fr>
2022-11-08 11:17:28 +0100

    ajout article 1

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const burger = document.querySelector('.burger');
 
 const navigation = document.querySelector('nav');
 
+const lien = document.querySelector('a');
+
 burger.addEventListener('click', ()=>{
     burger.classList.toggle('active');
-    navigation.classList.toggle('active')
+    navigation.classList.toggle('active'); 
+    lien.classList.toggle('active');
+    
 });
-
-
-
-
-
